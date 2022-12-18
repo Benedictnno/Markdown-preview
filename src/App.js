e3e4ad61ac75d2874662106d52b708091a6bff89
Benedictnno <100167694+Benedictnno@users.noreply.github.com>
2022-12-18 23:25:17 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import ReactMarkdown from "react-markdown";
 
 function App() {
+  const [input, setInput] = useState(
+    `## markdown preview 
+\n # H1 \n ## H2 \n ### H3 
+\n **bold text**\n *italicized text*
+ \n > blockquote \n 1. First item
+ \n 2. Second item \n3. Third item
+ \n- First item
+\n- Second item
+\n- Third item
+\n ---
+  \n [title](https://www.example.com)
+  \n ![image](https://cdn.pixabay.com/photo/2022/11/07/10/13/christmas-7576056_640.jpg)
+`
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <textarea
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <p className="result">
+        <ReactMarkdown>{input}</ReactMarkdown>
+      </p>
     </div>
   );
 }
